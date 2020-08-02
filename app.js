6bf5384d5f2b8c9862c8de884d3f73dc8c3ea535
SimoSbara <sbaraglia.simone@gmail.com>
2020-08-02 19:27:54 +0200

    Added port in mysql connection

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const connection = mysql.createConnection({
   host     : process.env.HOST,
   user     : process.env.USER,
   password : process.env.PASSWORD,
-  database : process.env.DATABASE
+  database : process.env.DATABASE,
+  port     : process.env.SQLPORT
   });
 
 connection.connect();
