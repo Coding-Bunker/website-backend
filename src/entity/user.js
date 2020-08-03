0d286235ea603f825dff6275d8a37c3c9796c799
SimoSbara <sbaraglia.simone@gmail.com>
2020-08-03 11:19:48 +0200

    Configuration of attributes in user entity

--- src/entity/user.js
+++ src/entity/user.js
@@ -3,23 +3,26 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 @Entity("user")
 export class User {
         @PrimaryGeneratedColumn()
-        id = undefined;
+        id = number;
 
-        @Column("varchar")
+        @Column("varchar", {length: 50})
         email = "";
 
         @Column("varchar", {
-                nullable: true
+                nullable: true,
+                length: 256
         })
         password = "";
 
         @Column("varchar", {
-                nullable: true
+                nullable: true,
+                length: 24
         })
         name = "";
 
         @Column("varchar", {
-                nullable: true
+                nullable: true,
+                length: 24
         })
         surname = "";
 }
