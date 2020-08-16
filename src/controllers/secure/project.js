40715414efd03e057f77273b6cb368a69d4054c3
SimoSbara <38192614+SimoSbara@users.noreply.github.com>
2020-08-16 14:43:47 +0200

    added CRUD functions to all entities

--- src/controllers/secure/project.js
+++ src/controllers/secure/project.js
@@ -4,16 +4,105 @@ import { Project } from '../../entity/project';
 
 export default {
 	getProjects: async (req, res) => {
-		const projectRepo = getRepository(Project);
+		const ProjectRepo = getRepository(Project);
 
-		const projects = await projectRepo.find();
+		const projects = await ProjectRepo.find();
 
-		res.status(200).json({
-			projects,
-		});
+		try {
+			res.status(200).json({
+				projects,
+			});
+		} catch (e) {
+			console.error(e);
+
+			return res.status(500).json({
+				ok: false,
+				message: 'internal error',
+			});
+		}
 	},
-	getProject: async (req, res) => {},
-	createProject: async (req, res) => {},
-	updateProject: async (req, res) => {},
-	deleteProject: async (req, res) => {},
+	getProject: async (req, res) => {
+		const projectID = req.params.id;
+
+		const ProjectRepo = getRepository(Project);
+
+		try {
+			const project = await ProjectRepo.findOne(projectID);
+
+			return res.status(200).json({
+				project,
+			});
+		} catch (e) {
+			console.error(e);
+
+			return res.status(500).json({
+				ok: false,
+				message: 'internal error',
+			});
+		}
+	},
+	createProject: async (req, res) => {
+		const ProjectRepo = getRepository(Project);
+
+		try {
+			const newProject = ProjectRepo.create({
+				...req.body,
+			});
+
+			await ProjectRepo.insert(newProject);
+
+			res.status(201).json({
+				ok: true,
+				project: newProject,
+			});
+		} catch (e) {
+			console.error(e);
+
+			return res.status(500).json({
+				ok: false,
+				message: 'internal error',
+			});
+		}
+	},
+	updateProject: async (req, res) => {
+		const projectID = req.params.id;
+		const ProjectRepo = getRepository(Project);
+
+		try {
+			await ProjectRepo.update(projectID, req.body);
+
+			const updatedProject = await ProjectRepo.findOne(projectID);
+
+			res.status(200).json({
+				ok: true,
+				project: updatedProject,
+			});
+		} catch (e) {
+			console.error(e);
+
+			return res.status(500).json({
+				ok: false,
+				message: 'internal error',
+			});
+		}
+	},
+	deleteProject: async (req, res) => {
+		const projectID = req.params.id;
+		const ProjectRepo = getRepository(Project);
+
+		try {
+			await ProjectRepo.delete(projectID);
+
+			res.status(200).json({
+				ok: true,
+			});
+		} catch (e) {
+			console.error(e);
+
+			return res.status(500).json({
+				ok: false,
+				message: 'internal error',
+			});
+		}
+	},
 };
