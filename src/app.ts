f0011966208e79158ebac27a84091ae999b10cb6
ArcaneDiver <michele.dellamea@bearzi.org>
2020-08-22 13:35:03 +0200

    refactor: 💡 useless console.log

--- src/app.ts
+++ src/app.ts
@@ -21,8 +21,6 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parse form encoded reques
 
 app.use(cookieParser());
 
-console.log(process.env.NODE_ENV);
-
 if (__DEV__) {
 	app.use(
 		morgan('dev', {
